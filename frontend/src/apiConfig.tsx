1861f8e9a548d617438c330e49a103e38f96e3a8
Aleksey Tertyshnyk <aleksey.tertyshnyk@winningtemp.com>
2023-04-22 14:15:57 +0300

    fixed API URL resolution

--- frontend/src/apiConfig.tsx
+++ frontend/src/apiConfig.tsx
@@ -1,4 +1,6 @@
+const API_URL = import.meta.env.MODE === "development" ? 'http://localhost:5244' : 'http://flashxcards-load-balancer-1208812410.eu-west-1.elb.amazonaws.com';
+
 export default
 {
-    baseUrl: "http://localhost:5244"
+    baseUrl: API_URL
 }
