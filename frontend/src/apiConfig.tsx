4b4a16abfb8ec9a2b66dea0b2ca439cb9c0d5d78
Aleksey Tertyshnyk <aleksey.tertyshnyk@winningtemp.com>
2023-04-23 20:16:05 +0300

    added propper HTTPS for API

--- frontend/src/apiConfig.tsx
+++ frontend/src/apiConfig.tsx
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.MODE === "development" ? 'http://localhost:5244' : 'http://flashxcards-load-balancer-1208812410.eu-west-1.elb.amazonaws.com';
+const API_URL = import.meta.env.MODE === "development" ? 'http://localhost:5244' : 'https://flashcardmate.net';
 
 export default
 {
